8f8a70f9da048d884baa8343c8b655db61c734eb
joalopez1 <204105785+joalopez1@users.noreply.github.com>
2025-07-23 11:28:50 -0300

    CRUD Usuarios

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cursosRoutes from "./routes/cursos.routes.js";
-
+import usuarioRoutes from "./routes/usuario.routes.js";
 
 const app = express();
 
 // Middlewares
 app.use(express.json()); 
 
+// Rutas
 app.use(cursosRoutes);
+app.use(usuarioRoutes);
 
 export default app;
--- backend/src/routes/usuario.routes.js
+++ backend/src/routes/usuario.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { usuarioController } from "../controllers/usuarios.controller.js";
 
 const routerUsuario = Router();
 
-routerUsuario.get("/cursos", (req, res) => {});
-routerUsuario.post("/cursos", (req, res) => {});
-routerUsuario.put("/cursos/:idCurso", (req, res) => {});
-routerUsuario.delete("/cursos/:idCurso", (req, res) => {});
-routerUsuario.get("/cursos/:idCurso", (req, res) => {});
+routerUsuario.get("/usuarios", usuarioController.getAllUsuarios);
+routerUsuario.post("/usuarios", usuarioController.createUsuario);
+routerUsuario.put("/usuarios/:idUsuario", usuarioController.updateUsuario);
+routerUsuario.delete("/usuarios/:idUsuario", usuarioController.deleteUsuario);
+routerUsuario.get("/usuarios/:idUsuario", usuarioController.getUsuarioById);
 
 export default routerUsuario;
