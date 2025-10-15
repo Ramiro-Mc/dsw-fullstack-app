e7323727b9428713f5e9800ef99f58353846cbc8
joalopez1 <204105785+joalopez1@users.noreply.github.com>
2025-10-15 13:38:49 -0300

    ya se puede crear un curso desde la pagina estando logueado, tambien completar y registrar el progreso de un alumno en un curso

--- backend/src/routes/pagos.routes.js
+++ backend/src/routes/pagos.routes.js
@@ -1,27 +1,27 @@
-// import { Router } from "express";
-// import { pagoController } from "../controllers/pagos.controller.js";
+// // import { Router } from "express";
+// // import { pagoController } from "../controllers/pagos.controller.js";
 
-// const routerPagos = Router();
+// // const routerPagos = Router();
 
-// routerPagos.get("/checkout/curso/:idCurso", pagoController.getCursoCheckout);
-// routerPagos.post("/pagos/crear-preferencia", pagoController.crearPreferencia);
-// routerPagos.post("/pagos/webhook", pagoController.webhook);
-// routerPagos.get("/pagos/verificar/:idUsuario/:idCurso", pagoController.verificarPago);
+// // routerPagos.get("/checkout/curso/:idCurso", pagoController.getCursoCheckout);
+// // routerPagos.post("/pagos/crear-preferencia", pagoController.crearPreferencia);
+// // routerPagos.post("/pagos/webhook", pagoController.webhook);
+// // routerPagos.get("/pagos/verificar/:idUsuario/:idCurso", pagoController.verificarPago);
 
-// export default routerPagos;
+// // export default routerPagos;
 
 
-// import { Router } from "express";
-// import { pagoController } from "../controllers/pagos.controller.js";
+// // import { Router } from "express";
+// // import { pagoController } from "../controllers/pagos.controller.js";
 
-// const routerPagos = Router();
+// // const routerPagos = Router();
 
-// // Cambiar la ruta para evitar conflictos
-// routerPagos.get("/pagos/checkout/curso/:idCurso", pagoController.getCursoCheckout);  // ← CAMBIO AQUÍ
-// routerPagos.post("/pagos/crear-preferencia", pagoController.crearPreferencia);
-// routerPagos.post("/pagos/webhook", pagoController.webhook);
+// // // Cambiar la ruta para evitar conflictos
+// // routerPagos.get("/pagos/checkout/curso/:idCurso", pagoController.getCursoCheckout);  // ← CAMBIO AQUÍ
+// // routerPagos.post("/pagos/crear-preferencia", pagoController.crearPreferencia);
+// // routerPagos.post("/pagos/webhook", pagoController.webhook);
 
-// export default routerPagos;
+// // export default routerPagos;
 
 
 import { Router } from "express";
